a218a14186784d522c076ec80f31d62203c34d77
hossein <hossein.h.a770227@gmail.com>
2024-08-01 22:56:48 +0330

    add .env

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,12 @@ env.read_env()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gz8jc!*sbnkb68f&srq-e9wwvp%_*i4y-$$=^j9tsgu*(&gi5d'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
